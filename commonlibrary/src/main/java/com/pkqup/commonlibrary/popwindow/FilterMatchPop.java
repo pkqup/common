7159e32827bd5c8abd60a3c9edd76a310e432170
wuqugame <wuqugame@anzogame.com>
2018-08-22 11:42:19 +0800

    添加代码

---
+++ commonlibrary/src/main/java/com/pkqup/commonlibrary/popwindow/FilterMatchPop.java
@@ -0,0 +1,113 @@
+package com.pkqup.commonlibrary.popwindow;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import com.pkqup.commonlibrary.R;
+
+import java.util.List;
+
+
+/**
+ * @CreatedbBy: liucun on 2018/8/7
+ * @Describe:
+ */
+public class FilterMatchPop extends PopupWindow {
+
+    private Context context;
+    private LayoutInflater inflater;
+    private View mContentView;
+
+
+    /**
+     * 适配7.0系统Popwindow显示全屏的问题
+     * @param anchor
+     */
+    @Override
+    public void showAsDropDown(View anchor) {
+        if(Build.VERSION.SDK_INT >= 24) {
+            Rect rect = new Rect();
+            anchor.getGlobalVisibleRect(rect);
+            int h = anchor.getResources().getDisplayMetrics().heightPixels - rect.bottom;
+            setHeight(h);
+        }
+        super.showAsDropDown(anchor);
+    }
+
+    /**
+     * 适配7.0系统Popwindow显示全屏的问题
+     * @param anchor
+     */
+    @Override
+    public void showAsDropDown(View anchor, int xoff, int yoff) {
+        if(Build.VERSION.SDK_INT >= 24) {
+            Rect rect = new Rect();
+            anchor.getGlobalVisibleRect(rect);
+            int h = anchor.getResources().getDisplayMetrics().heightPixels - rect.bottom;
+            setHeight(h);
+        }
+        super.showAsDropDown(anchor, xoff, yoff);
+    }
+
+    public FilterMatchPop(Context context) {
+        super(context);
+        this.context = context;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mContentView = inflater.inflate(R.layout.popwindow_class, null);
+        setContentView(mContentView);
+
+        //设置宽与高
+        setWidth(WindowManager.LayoutParams.MATCH_PARENT);
+        setHeight(WindowManager.LayoutParams.MATCH_PARENT);
+
+        setBackgroundDrawable(new ColorDrawable());
+        setFocusable(true);
+        setOutsideTouchable(true);
+        setTouchable(true);
+
+        setTouchInterceptor(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return event.getAction() == MotionEvent.ACTION_OUTSIDE;
+            }
+        });
+        initView();
+    }
+
+    private void initView() {
+        LinearLayout llPopWindow =  mContentView.findViewById(R.id.ll_popWindow);
+        llPopWindow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+
+
+    private OnCallBackListener onCallBackListener;
+
+    public interface OnCallBackListener {
+        void onItemClick();
+    }
+
+    public void setCallBackListener(OnCallBackListener onCallBackListener) {
+        this.onCallBackListener = onCallBackListener;
+    }
+
+
+
+}
