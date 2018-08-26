a6648d5b49da1bdc3a09e8998c1c82f6c6e31249
liucun <a123>
2018-08-27 00:56:55 +0800

    添加代码

--- commonlibrary/src/main/java/com/pkqup/commonlibrary/net/bean/ResultBean.java
+++ commonlibrary/src/main/java/com/pkqup/commonlibrary/net/bean/ResultBean.java
@@ -1,7 +1,18 @@
 package com.pkqup.commonlibrary.net.bean;
 
 
+import com.google.gson.annotations.SerializedName;
+
 public class ResultBean<T>{
+
+    private String appid;
+    private String noncestr;
+    @SerializedName("package")
+    private String packageName;
+    private String partnerid;
+    private String prepayid;
+    private String timestamp;
+    private String sign;
 
     private int errorcode;
     private String msg;
@@ -29,5 +40,61 @@ public class ResultBean<T>{
 
     public void setMsg(String msg) {
         this.msg = msg;
+    }
+
+    public String getAppid() {
+        return appid;
+    }
+
+    public void setAppid(String appid) {
+        this.appid = appid;
+    }
+
+    public String getNoncestr() {
+        return noncestr;
+    }
+
+    public void setNoncestr(String noncestr) {
+        this.noncestr = noncestr;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getPartnerid() {
+        return partnerid;
+    }
+
+    public void setPartnerid(String partnerid) {
+        this.partnerid = partnerid;
+    }
+
+    public String getPrepayid() {
+        return prepayid;
+    }
+
+    public void setPrepayid(String prepayid) {
+        this.prepayid = prepayid;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
     }
 }
